4af3e380f507c1d2a6b0bfc066abc33a070ea9f9
jiang <jianglinair@163.com>
2021-02-02 08:34:52 +0800

    export a

---
+++ exporta/src/main/java/com/jianglin/exporta/ARouterPath.java
@@ -0,0 +1,12 @@
+package com.jianglin.exporta;
+
+/**
+ * Author: Jianglin
+ * Time: 2021/2/2
+ * Description: todo
+ *
+ * @author jiang
+ */
+public class ARouterPath {
+    public static final String A = "a/main";
+}
